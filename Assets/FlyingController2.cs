7090a4c80127f42cc20009bc9f5a5a2dd6bb3817
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-03-27 09:12:21 +0530

    workink on the speed controller

--- Assets/FlyingController2.cs
+++ Assets/FlyingController2.cs
@@ -1,28 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FlyingController2 : MonoBehaviour
 {
 
-    public float thrust = 10f;
+    public float thrust;
     public float topSpeed = 20f;
     public float defaulLift = 1f;
+    float dynamicLift;
+    float fallRate;
+    public Slider thrustSlider;
+
 
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        thrustSlider.onValueChanged.AddListener(delegate {ValueChangeCheck();});
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-       
-        rb.velocity = transform.forward * thrust;
-        transform.Rotate(defaulLift, 0, 0);
+    
+        
+        transform.Translate(0, 0, thrust * Time.deltaTime);
+        transform.Rotate(dynamicLift, 0, 0);
+
+        rb.velocity = new Vector3(0,rb.velocity.y * fallRate ,0);
 
         if (Input.GetKey(KeyCode.W))
         {
@@ -42,5 +50,21 @@ public class FlyingController2 : MonoBehaviour
             transform.Rotate(0, 0, -0.5f);
         }
 
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -0.5f, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, 0.5f, 0);
+        }
+
+    }
+
+     public void ValueChangeCheck()
+    {
+        thrust = thrustSlider.value * topSpeed;
+        dynamicLift = thrustSlider.value * defaulLift;
+        fallRate = 1f - thrustSlider.value;
     }
 }
