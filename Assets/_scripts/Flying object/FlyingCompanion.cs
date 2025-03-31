9d80a948ecfeb4ebf25fb2cee438a61f403f8563
bhusanSahoo <bhusan12sahoo@gmail.com>
2025-03-31 18:29:28 +0530

    Polishing part is done here

--- Assets/_scripts/Flying object/FlyingCompanion.cs
+++ Assets/_scripts/Flying object/FlyingCompanion.cs
@@ -9,11 +9,7 @@ public class FlyingCompanion : MonoBehaviour
     public float gap = 1f;
     public Transform player;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+ 
 
     // Update is called once per frame
     void Update()
